f26cb22afc6c88b916427ca6a115c14bcf628605
andres <asdas>
2013-10-28 13:43:22 -0500

    imaws

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Eduapp::Application.routes.draw do
-  root to: 'static_pages#home'
+  root :to =>'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
